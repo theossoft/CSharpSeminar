a2c8df6969739382649cf8bebee2876137a28dbb
theos <theossoft@gmail.com>
2023-02-11 17:24:35 +0300

    Added task01 and task03

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 0. Демонстрация решения
+// Напишите программу, которая на вход
+// принимает число и выдаёт его квадрат (число
+// умноженное на само себя).
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int square = number * number;
+Console.WriteLine("Квадрат числа равен: " + (number * number));
+Console.WriteLine($"Квадрат числа равен: {number} = {square}");
---
+++ Task03/Program.cs
@@ -0,0 +1,22 @@
+﻿// 3. Напишите программу, которая будет выдавать
+// название дня недели по заданному номеру.
+// 3 -> Среда
+// 5 -> Пятница
+
+Console.WriteLine("Задача 5");
+string wensday = "Среда";
+string friday = "Пятница";
+
+int number = Convert.ToInt32(Console.ReadLine());
+if (number == 3)
+{
+    Console.WriteLine(wensday);
+}
+else if (number == 5)
+{
+    Console.WriteLine(friday);
+}
+else
+{
+    Console.WriteLine("Нет такого дня");
+}
