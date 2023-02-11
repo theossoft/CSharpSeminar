fb9548a3195b0b06fc66712952ac9a3a340d5040
theos <theossoft@gmail.com>
2023-02-11 17:54:59 +0300

    Решена 5 задача

--- Task05/Program.cs
+++ Task05/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+
+if (number > 0) {count = number * (-1);} else
+{
+    count = number;
+    number = number * (-1);
+}
+
+while(count <= number) {
+    Console.Write(count + " ");
+    count++;
+}
