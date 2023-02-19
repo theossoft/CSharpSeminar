f32489fae3d57dc7ca543152602ae3bc6d79bc86
theos <theossoft@gmail.com>
2023-02-19 17:16:29 +0300

    Задача 17 решена

--- Task17/Program.cs
+++ Task17/Program.cs
@@ -2,3 +2,22 @@
 // координаты точки (X и Y), причем X!=0 и Y!=0 и выдает
 // номер четверти плоскости, в которой находится эта точка.
 
+Console.WriteLine("Введите координаты точки");
+Console.Write("X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+int quarter = Quarter(x, y);
+string result = quarter > 0 
+                ? $"Указанные координаты соответсвуют четверти {quarter}"
+                : "Введены некорректные координаты";
+Console.WriteLine(result);                
+
+int Quarter(int xc, int yc) {
+    if(xc > 0 && yc > 00) return 1;
+    if(xc < 0 && yc > 00) return 2;
+    if(xc < 0 && yc < 00) return 3;
+    if(xc > 0 && yc < 00) return 4;
+    return 0;
+}
