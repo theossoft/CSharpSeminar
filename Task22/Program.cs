9bebcafb9693735741a90850ad9455b5ec9e32a6
theos <theossoft@gmail.com>
2023-02-20 01:24:31 +0300

    Задача 22 решена

--- Task22/Program.cs
+++ Task22/Program.cs
@@ -2,3 +2,11 @@
 // выдает таблицу квадратов числел от 1 до N.
 // 5 -> 1, 4, 9, 16, 25
 // 2 -> 1, 4
+
+Console.Write("Введите число: ");
+int lastNum = Convert.ToInt32(Console.ReadLine());
+
+for(int i = 1; i <= lastNum; i++) {
+    Console.Write(i + "   ");
+    Console.WriteLine(i * i);
+}
