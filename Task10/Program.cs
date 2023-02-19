b2829743e31e4d6df1fa6771ad6d987052ac5e21
theos <theossoft@gmail.com>
2023-02-19 15:28:11 +0300

    Что-то массово изменилось

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -6,8 +6,26 @@
 // 782 -> 8
 // 918 -> 1
 
-Console.Write("Введите трехзначное число: ");
-int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите трехзначное число: ");
+// int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+// Console.Write($"Вторая цифра в трехзначном числе = {(threeDigitNumber % 100) / 10}");
 
-Console.Write($"Вторая цифра в трехзначном числе = {(threeDigitNumber % 100) / 10}");
+// Пример идеального решения
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return;
+}
 
+int number = Prompt("Введите трехзначное число > ");
+if (number < 100 || number >= 1000)
+{
+    Console.WriteLine("Вы ввели не трехзначное число, пожалуйста повторите ввод");
+    return;
+}
+
+Console.WriteLine($"Введенное число `{number}`");
+int secondRank = number / 10 % 10;
+Console.WriteLine($"Вторая цифра `{secondRank}`");
