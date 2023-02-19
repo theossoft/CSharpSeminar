4f08e0909fe42543ccc2027405b66bd3eccfa2fe
theos <theossoft@gmail.com>
2023-02-19 11:38:01 +0300

    Задача 13 решена

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -5,3 +5,20 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
+
+
+Console.Write("Введите трехзначное число: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+
+if (threeDigitNumber < 100)
+{
+    Console.WriteLine("Третьей цифры нет.");
+}
+else
+{
+    while (threeDigitNumber > 999)
+    {
+        threeDigitNumber = threeDigitNumber / 10;
+    }
+    Console.WriteLine($"Третья цифра трехзначного числа {threeDigitNumber} = {threeDigitNumber % 10}");
+}
