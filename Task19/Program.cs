5bdbfd31e7f0f8ec3dc118d8216b7c99c3b715b8
theos <theossoft@gmail.com>
2023-02-20 23:31:56 +0300

    Задача 19 решена

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -3,3 +3,20 @@
 // 14212 -> нет
 // 12821 -> да
 // 23432 -> да
+
+Console.Write("Введите пятизначное число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+if (x < 10000 || x > 99999)
+{
+    Console.Write("Это не пятизначное число!");
+    return;
+}
+
+if (Palindrom5Digits(x)) Console.Write("ДА");
+else Console.Write("НЕТ");
+
+bool Palindrom5Digits(int number)
+{
+    if ((number / 10000 == number % 10) && (number / 1000 % 10 == number / 10 % 10)) { return true; }
+    return false;
+}
