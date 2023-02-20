a724d5d9117fc0cf5a00f24ca4e8bac13a2e611a
theos <theossoft@gmail.com>
2023-02-20 23:46:55 +0300

    Задача 21 решена

--- Task21/Program.cs
+++ Task21/Program.cs
@@ -3,4 +3,17 @@
 // A(3,6,8); B(2,1,-7) -> 15.84
 // A(7,-5,0); B(1,-1,9) -> 11.53
 
+Console.Write("Введите X точки A: ");
+int ax = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Y точки A: ");
+int ay = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Z точки A: ");
+int az = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите X точки B: ");
+int bx = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Y точки B: ");
+int by = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Z точки A: ");
+int bz = Convert.ToInt32(Console.ReadLine());
 
+Console.WriteLine($"Расстояние между точками А и В: {Math.Round(Math.Sqrt(Math.Pow((bz - az), 2) + Math.Pow((bx - ax), 2) + Math.Pow((by - ay), 2)),2,MidpointRounding.ToZero)}");
