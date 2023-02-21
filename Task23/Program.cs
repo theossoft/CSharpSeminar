25cf4ff8d34df7c994b5712f1244db07972a3bd1
theossoft <theossoft@gmail.com>
2023-02-21 10:31:59 +0300

    Задача 23 решена.

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -2,3 +2,11 @@
 // и выдает таблицу кубов чисел от 1 до N.
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
+
+Console.Write("Введите число: ");
+int lastNum = Convert.ToInt32(Console.ReadLine());
+
+for(int i = 1; i <= lastNum; i++) {
+    Console.Write(i + "   ");
+    Console.WriteLine(i * i * i);
+}
