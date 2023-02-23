671e86bb04d94aab918749c3c1f21a5100e00004
theos <theossoft@gmail.com>
2023-02-23 22:49:38 +0300

    Задача 24 решена

--- Task24/Program.cs
+++ Task24/Program.cs
@@ -4,3 +4,22 @@
 // 4 -> 10
 // 8 -> 36
 
+Console.WriteLine("Введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = SumNumbers(number);
+Console.WriteLine($"Сумма чисел от 1 до {number} = {sum}");
+
+int SumNumbers(int num)
+{
+    int sum = 0;
+    for (int i = 0; i < num; i++)
+    {
+        checked
+        {
+            sum = sum + i;
+        }
+    }
+
+    return sum;
+}
+
