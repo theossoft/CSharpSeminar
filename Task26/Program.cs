1bb6af2d07f2e4615ee0e3e8ee23b7668abf50ae
theos <theossoft@gmail.com>
2023-02-23 23:38:59 +0300

    Задача 26 решена

--- Task26/Program.cs
+++ Task26/Program.cs
@@ -3,3 +3,20 @@
 // 456 -> 3
 // 78 -> 2
 // 89126 -> 5
+
+Console.Write("Введите любое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int numCount(int num)
+{
+    int count = 0;
+    while (num != 0)
+    {
+        count++;
+        num = num / 10;
+        
+    }
+    return count;
+}
+
+Console.WriteLine(numCount(number));
