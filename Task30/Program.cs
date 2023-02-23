d5982cdddca0adc76f0a49fc95256927da858391
theos <theossoft@gmail.com>
2023-02-24 00:31:59 +0300

    Задача 30 решена

--- Task30/Program.cs
+++ Task30/Program.cs
@@ -2,3 +2,25 @@
 // из 8 элементов, заполненный  нулями и единицами
 // в случайном порядке.
 // [1,0,1,1,0,]
+
+int[] NewRandomArray(int num)
+{
+    Random random = new Random();
+    int[] arr = new int[num];
+    for (int i = 0; i < num; i++)
+    {
+        arr[i] = random.Next(0,2);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+}
+
+
+PrintArray(NewRandomArray(10));
