f11cb79c732e414da3ec4c39eb1834273bc3881d
theos <theossoft@gmail.com>
2023-02-23 23:50:02 +0300

    Задача 28 решена

--- Task28/Program.cs
+++ Task28/Program.cs
@@ -1,4 +1,32 @@
-﻿// 28. Здесь должно быть условие задачи
+﻿// Задача 28. Напишите программу, которая принимает на вход
+// число N и выдаёт произведение чисел от 1 до N
+// 4 -> 24
+// 5 -> 120
 
-Console.Write("Введите число: ");
-int lastNum = Convert.ToInt32(Console.ReadLine()); 
+Console.Write("Введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0)
+{
+    int multiply = Factorial(number);
+    Console.Write($"Факториал числа {number} = {multiply}");
+}
+else
+{
+    Console.WriteLine("Ошибка ввода - число не натуральное!");
+}
+
+
+int Factorial(int num)
+{
+    int sum = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        checked
+        {
+            sum = sum * i;
+        }
+    }
+
+    return sum;
+}
