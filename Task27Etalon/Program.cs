6db0af6475efe1ed2b4f09125a5d674db3221441
theos <theossoft@gmail.com>
2023-02-24 23:06:32 +0300

    Задача 27. Найдено эталонное решение

--- Task27Etalon/Program.cs
+++ Task27Etalon/Program.cs
@@ -3,3 +3,23 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+
+int Prompt(string message)
+{
+    System.Console.Write(message); // Выводим приглашение ко вводу
+    string readInput = System.Console.ReadLine(); // Выводим значение
+    int result = int.Parse(readInput); // Приводим к числу
+    return result; // возвращаем результат
+}
+
+int SumAllDigit(int number) {
+    int result = 0;
+    while (number > 0) {
+        result += number % 10;
+        number = number / 10;
+    }
+    return result;
+}
+
+int number = Prompt("Введите число: ");
+System.Console.WriteLine($"Сумма всех чисел в цифре {number} = {SumAllDigit(number)}");
