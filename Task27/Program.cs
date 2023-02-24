a3fbb9d8e5951531f3277c38a7e9509a70c502d2
theos <theossoft@gmail.com>
2023-02-24 22:34:46 +0300

    Задача 27 решена

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -3,3 +3,20 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+
+Console.Write("Введите любое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumOfNum(int num)
+{
+    int sum = 0;
+    while (num != 0)
+    {
+        sum = sum + num%10;
+        num = num / 10;
+    }
+    return sum;
+}
+
+int summa = SumOfNum(number);
+Console.Write($"Сумма цифр заданного числа = {summa}");
