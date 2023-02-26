e496dbc60723c6e744b5ad99ba532af691e419cc
theos <theossoft@gmail.com>
2023-02-26 12:22:18 +0300

    Задача 36 решена

--- Task36/Program.cs
+++ Task36/Program.cs
@@ -4,3 +4,41 @@
 
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
+
+int[] CreateArrayRandomInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write(arr[i] + ",");
+        else Console.Write(arr[i]);
+    }
+    Console.WriteLine("]");
+}
+
+int ArraySumOddNumber(int[] arr)
+{
+    int sum = 0;
+    for (int i = 1; i < arr.Length; i += 2)
+    {
+        sum = sum + arr[i];
+    }
+    return sum;
+}
+
+int[] randomArray = CreateArrayRandomInt(6, 0, 10);
+PrintArray(randomArray);
+int arraySumOddNumber = ArraySumOddNumber(randomArray);
+Console.WriteLine($"Сумма нечетных элементов массива = {arraySumOddNumber}");
+
