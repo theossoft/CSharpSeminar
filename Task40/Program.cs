95c6cb8017f7148e8281402f538ff83fbaa0c666
theos <theossoft@gmail.com>
2023-02-28 20:43:48 +0300

    Задача 40 решена

--- Task40/Program.cs
+++ Task40/Program.cs
@@ -3,3 +3,24 @@
 // такой длины.
 // Каждая сторона треугольника меньше суммы двух других сторон.
 
+Console.Write("Введите длину стороны треугольника: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите длину стороны треугольника: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите длину стороны треугольника: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+
+bool isTreangleExists(int x, int y, int z)
+{
+    if (a + b > c && b + c > a && c + a > b)
+        return true;
+    else
+        return false;
+}
+
+
+if (isTreangleExists(a, b, c))
+    Console.WriteLine("Треугольник существует.");
+else
+    Console.WriteLine("Треугольник НЕсуществует.");
