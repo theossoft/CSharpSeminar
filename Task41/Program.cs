d610e17b5aafb4e31d6d79d83b6434a7dcd51711
theos <theossoft@gmail.com>
2023-03-05 22:09:10 +0300

    Задача 41 решена

--- Task41/Program.cs
+++ Task41/Program.cs
@@ -2,3 +2,20 @@
 // Посчитайте, сколько чисел больше 0 ввёл пользователь.
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите количество чисел для ввода: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int CounPositiveNumber(int num)
+{
+    int count = 0;
+    for (int i = 0; i < num; i++)
+    {
+        Console.Write("Введите число: ");
+        if (Convert.ToInt32(Console.ReadLine()) > 0) count++;
+    }
+    return count;
+}
+
+int a = CounPositiveNumber(number);
+Console.WriteLine(a);
