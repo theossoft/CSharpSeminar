7e80bd8682ff723cabddfc66fea0f6d1a7d5a24a
theos <theossoft@gmail.com>
2023-03-05 21:23:51 +0300

    Задача 39 решена

--- Task39/Program.cs
+++ Task39/Program.cs
@@ -3,3 +3,40 @@
 // последнем и т.д)
 // [1 2 3 4 5] -> [5 4 3 2 1]
 // [6 7 3 6] -> [6 3 7 6]
+
+int[] CreateArrayRandomInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write(arr[i] + ",");
+        else Console.Write(arr[i]);
+    }
+    Console.WriteLine("]");
+}
+
+void ReverseArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length / 2; i++)
+    {
+        int temp = arr[i];
+        arr[i] = arr[arr.Length - 1 - i];
+        arr[arr.Length - 1 - i] = temp;
+    }
+}
+
+int[] array = CreateArrayRandomInt(5, 1, 10);
+PrintArray(array);
+ReverseArray(array);
+PrintArray(array);
