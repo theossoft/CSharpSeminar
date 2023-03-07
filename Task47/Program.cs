85de9ca699203b604a2b9b592c68516aa6cd2422
theos <theossoft@gmail.com>
2023-03-07 21:59:21 +0300

    Задача 47 решена

--- Task47/Program.cs
+++ Task47/Program.cs
@@ -5,3 +5,37 @@
 // 0,5 7 -2 -0,2
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
+
+double[,] CreateMatrixRndInt(int rows, int columns, double min, double max)
+{
+    double[,] matrix = new double[rows, columns]; // 0 ,  1
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++) // rows
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // columns
+        {
+            // matrix[i, j] = rnd.NextDouble(min, max + 1);
+            matrix[i, j] = Math.Round((rnd.NextDouble() * (max - min) + min), 2);
+        }
+    }
+    return matrix;
+}
+
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],6}, ");
+            else Console.Write($"{matrix[i, j],6}");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+double[,] array2d = CreateMatrixRndInt(5, 6, -100.0, 100.0);
+PrintMatrix(array2d);
