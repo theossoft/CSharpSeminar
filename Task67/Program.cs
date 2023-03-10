a1d61b95e65c9972550e4cc63b60e54d77481ac7
theos <theossoft@gmail.com>
2023-03-10 21:13:16 +0300

    Задача 67 решена

--- Task67/Program.cs
+++ Task67/Program.cs
@@ -2,15 +2,14 @@
 // возвращать сумму его цифр.
 // 453 -> 12
 // 45 -> 9
-// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и
-// возводит число А в целую степень B с помощью рекурсии.
-// A = 3; B = 5 -> 243 (3⁵)
-// A = 2; B = 3 -> 8
 
-int FactorialRec(int n)
- {
-     if (n == 1) return 1;
-     else return n * FactorialRec(n - 1);
- }
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
 
- Console.WriteLine(FactorialRec(10)); // 3628800
+int SumDigits(int n)
+{
+    if (n == 0) return 0;
+    else return n % 10 + SumDigits(n / 10);
+}
+
+Console.WriteLine(SumDigits(number));
