59aa80e0dca182783b4c28bbda2a00a03b4078b6
theos <theossoft@gmail.com>
2023-03-11 23:30:49 +0300

    Задача 57 решена

--- Task57/Program.cs
+++ Task57/Program.cs
@@ -33,3 +33,36 @@ void PrintMatrix(int[,] matrix)
     }
 }
 
+void SumEachElementMatrix(int[,] matrix)
+{
+    int c = 0;
+    int[] array = new int[matrix.Length];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            array[c] = matrix[i, j];
+            c += 1;
+        }
+    }
+    Array.Sort(array);
+    int count = 1;
+    int currentNumber = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] == currentNumber) count++;
+        else
+        {
+            Console.WriteLine($"Число {currentNumber} встречается {count} раз.");
+            currentNumber = array[i];
+            count = 1;
+        }
+    }
+    Console.WriteLine($"Число {currentNumber} встречается {count} раз.");
+}
+
+Console.WriteLine("Вывод случайно сгенерированной матрицы:");
+int[,] matrix = CreateMatrixRndInt(4, 4, 0, 10);
+PrintMatrix(matrix);
+Console.WriteLine("Нахождение суммы каждого элемента:");
+SumEachElementMatrix(matrix);
