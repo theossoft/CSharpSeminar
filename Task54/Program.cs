aff9f60916f960f028f5661adfc72f98e8a78aaa
theossoft <theossoft@gmail.com>
2023-03-13 14:09:02 +0300

    Задача 54 решена

--- Task54/Program.cs
+++ Task54/Program.cs
@@ -1,4 +1,6 @@
-﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+﻿// Задача 54: Задайте двумерный массив. 
+// Напишите программу, которая упорядочит по убыванию элементы 
+// каждой строки двумерного массива.
 // Например, задан массив:
 // 1 4 7 2
 // 5 9 2 3
@@ -7,3 +9,78 @@
 // 7 4 2 1
 // 9 5 3 2
 // 8 4 4 2
+
+void BubbleSort(int[] mas)
+{
+    int temp;
+    for (int i = 0; i < mas.Length; i++)
+    {
+        for (int j = i + 1; j < mas.Length; j++)
+        {
+            if (mas[i] > mas[j])
+            {
+                temp = mas[i];
+                mas[i] = mas[j];
+                mas[j] = temp;
+            }
+        }
+    }
+}
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns]; // 0 ,  1
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++) // rows
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // columns
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],3}, ");
+            else Console.Write($"{matrix[i, j],3}");
+        }
+        Console.WriteLine(" |");
+    }
+}
+
+int[,] SortedMatrix(int[,] matrix)
+{
+    int[] tempArray = new int[matrix.GetLength(0)];
+    int[,] tempMatrix = new int[matrix.GetLength(0), matrix.GetLength(1)];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            tempArray[j] = matrix[i, j];
+        }
+        BubbleSort(tempArray);
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            tempMatrix[i, matrix.GetLength(1) - 1 - j] = tempArray[j];
+        }
+    }
+    return tempMatrix;
+}
+
+System.Console.WriteLine("Сгенерирована случайная матрица:");
+int[,] newMatrix = CreateMatrixRndInt(4, 4, 0, 10);
+PrintMatrix(newMatrix);
+Console.WriteLine("--------------------------");
+System.Console.WriteLine("Строки матрицы упорядочены по убыванию:");
+int[,] sortedMatrix = SortedMatrix(newMatrix);
+PrintMatrix(sortedMatrix);
+
--- Task58/Program.cs
+++ Task58/Program.cs
@@ -5,3 +5,61 @@
 // Результирующая матрица будет:
 // 18 20
 // 15 18
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns]; // 0 ,  1
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++) // rows
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // columns
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],3}, ");
+            else Console.Write($"{matrix[i, j],3}");
+        }
+        Console.WriteLine(" |");
+    }
+}
+
+int[,] MultiplyTwoMatrix(int[,] matrixA, int[,] matrixB)
+{
+    int iMatrixA = 0;
+    int jMatrixA = 0;
+    int iMatrixB = 0;
+    int jMatrixB = 0;
+
+    int[,] multiplyMatrix = new int[matrixA.GetLength(0), matrixB.GetLength(1)];
+    for (int i = 0; i < multiplyMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; multiplyMatrix.GetLength(1); j++)
+        {
+multiplyMatrix[i,j] = (matrixA[jMatrixA, iMatrixA] * matrixB[iMatrixB, jMatrixB]);
+        }
+    }
+    return multiplyMatrix;
+}
+
+System.Console.WriteLine("Сгенерированы две случайные матрицы:");
+int[,] matrixA = CreateMatrixRndInt(4, 4, 0, 10);
+PrintMatrix(matrixA);
+Console.WriteLine("---------------------");
+int[,] matrixB = CreateMatrixRndInt(4, 4, 0, 10);
+PrintMatrix(matrixB);
+Console.WriteLine("Произведение двух случайных матриц:");
+
+
