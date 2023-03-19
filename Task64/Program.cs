8d51ee19fd3a97db60cc999fcda144aea04c08d9
theos <theossoft@gmail.com>
2023-03-19 14:25:12 +0300

    Задача 64 решена

--- Task64/Program.cs
+++ Task64/Program.cs
@@ -4,3 +4,11 @@
 
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+string SumNumber(int n)
+{
+    if (n > 0) return $"{n} " + SumNumber(n - 1);
+    else return String.Empty;
+}
+System.Console.WriteLine(SumNumber(10));
+
